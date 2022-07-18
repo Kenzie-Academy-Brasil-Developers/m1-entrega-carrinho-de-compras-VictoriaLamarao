cb46e399410279b78130c832133c119c21969023
Victoria Lamarao <victoria.slamarao@hotmail.com>
2022-07-18 09:40:03 -0300

    carrinho de compras

--- index.js
+++ index.js
@@ -26,9 +26,10 @@ const productsCart = [
   },
 ];
 
-let body = document.querySelector('carrinho')
 
-/* let main = document.createElement("main");
+let body = document.querySelector("body");
+
+let main = document.createElement("main");
 main.classList.add("principal");
 
 let h1 = document.createElement("h1");
@@ -37,22 +38,26 @@ h1.classList.add("titulo");
 let ul = document.createElement("ul");
 ul.classList.add("lista-produtos");
 
-let liItens = document.createElement("li");
-liItens.classList.add("produtos-itens"); */
+let cabecario = document.createElement("li");
+cabecario.classList.add("produtos-itens");
 
-/* let total = document.createElement("li");
-total.classList.add("produtos");
+let total = document.createElement("li");
+total.classList.add("produtos-itens2");
 
 let button = document.createElement("button");
-button.classList.add("botao"); */
+button.classList.add("botao");
 
+h1.innerText = "Virtual Market";
+button.innerText = "Finalizar compra";
+cabecario.innerHTML = "<p>Item</p><p>Valor</p>";
+total.innerHTML = `<p>Total</p><p> R$ ${somaProdutos(productsCart)}</p>`;
 
 
 function criarListas(objeto) {
   let nome = objeto.name;
   let preco = objeto.price;
 
-  let li = document.createElement(li);
+  let li = document.createElement("li");
   li.classList.add("produtos");
 
   let pNome = document.createElement("p");
@@ -61,13 +66,36 @@ function criarListas(objeto) {
   let pPreco = document.createElement("p");
   pPreco.classList.add("preco-produto");
 
-  pNome.innerText = nome
-  pPreco.innerText = preco
+  pNome.innerText = nome;
+  pPreco.innerText = "R$ " + preco;
+  
+ 
+  li.append(pNome, pPreco);
 
-  li.append(pNome,pPreco)
+  return li;
+}
+function somaProdutos(productsCart) {
+  let preco = productsCart.price;
+  let resultado = 0;
+  for (let i = 0; i < productsCart.length; i++) {
+    resultado += productsCart[i].price;
+  }
+  return resultado;
 }
 
-console.log(criarListas(productsCart));
+function passarLi(productsCart){
+  for (let i = 0; i < productsCart.length; i++) {
+    let objeto = productsCart[i];
+    let objeto2 = criarListas(objeto);
+
+    ul.append(objeto2);
+    console.log(objeto);
+  }
+}
+
+main.append(h1, ul, total, button);
 body.append(main);
-main.append(h1, ul, button);
-ul.append(criarListas(productsCart[0]));
+ul.append(cabecario);
+passarLi(productsCart);
+
+
